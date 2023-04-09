e460720a84675f5b0f5245a012ad07588b5d929a
torrixo <santy.torrico@gmail.com>
2023-04-09 11:17:26 -0400

    Added firestore to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { getAnalytics } from "firebase/analytics";
+import {provideFirestore,getFirestore} from '@angular/fire/firestore'
 
 @NgModule({
   declarations: [
@@ -25,7 +25,7 @@ import { getAnalytics } from "firebase/analytics";
     FormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase())
+    provideFirestore(() => getFirestore())
   ],
   providers: [MenuService],
   bootstrap: [AppComponent]

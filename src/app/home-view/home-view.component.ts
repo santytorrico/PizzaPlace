5ceb3d459e13f6001c6cfd24461cdf1207567bee
Leonardo Orsi <leonardo.orsi@ucb.edu.bo>
2023-05-17 12:09:00 -0400

    mejora de home page

--- src/app/home-view/home-view.component.ts
+++ src/app/home-view/home-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-home-view',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-view.component.scss']
 })
 export class HomeViewComponent {
+  whoAreWe: boolean = false;
+  whatWeDo: boolean = false;
+  mision: boolean = false;
+  contactanos: boolean = false;
+  vision: boolean = false;
+  equipo: boolean = false;
+  constructor(private location: Location){
+
+  }
+
+  QuienesSomos(){
+    this.whoAreWe=true;
+  }
+  QueHacemos(){
+    this.whatWeDo=true;
+  }
+  OurMission(){
+    this.mision=true;
+  }
+  contactUs(){
+    this.contactanos=true;
+  }
+  OurVision(){
+    this.vision=true;
+  }
+  OurTeam(){
+    this.equipo=true;
+  }
+  goBack(){
+    this.location.back();
+  }
 
 }
